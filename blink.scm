2b79855b79c6c9cbc3e22d603ad5fc190fbb1bbf
Aaron Patterson <aaron.patterson@gmail.com>
2013-04-18 16:06:50 -0700

    randomized colors

--- blink.scm
+++ blink.scm
@@ -1,42 +1,55 @@
-(use usb srfi-1)
+(module blink
+  (blink-devices
+   blink-open
+   blink-color!)
+  (import scheme chicken ports usb srfi-1 srfi-69)
+  (use usb srfi-1 srfi-69)
 
-(define (pack buf)
-  (with-output-to-string (lambda ()
-      (for-each (lambda (byte) (write-char (integer->char byte))) buf))))
+  (define (pack buf)
+    (with-output-to-string (lambda ()
+        (for-each (lambda (byte) (write-char (integer->char byte))) buf))))
 
-(define (blink-write handle buf)
-  (let ((request-type (bitwise-ior usb::request-type-class 
-                                   usb::recipient-device
-                                   usb::endpoint-out))
-        (request #x09)
-        (value (bitwise-ior (arithmetic-shift 3 8)
-                            (bitwise-and (car buf) #xFF)))
-        (index 0)
-        (bytes (pack buf))
-        (m 5000))
-    (usb-control-transfer handle
-                     request-type
-                     request
-                     value
-                     index
-                     bytes
-                     m)))
+  (define (blink-write handle buf)
+    (let ((request-type (bitwise-ior usb::request-type-class
+                                     usb::recipient-device
+                                     usb::endpoint-out))
+          (request #x09)
+          (value (bitwise-ior (arithmetic-shift 3 8)
+                              (bitwise-and (car buf) #xFF)))
+          (index 0)
+          (bytes (pack buf))
+          (m 5000))
+      (usb-control-transfer handle
+                       request-type
+                       request
+                       value
+                       index
+                       bytes
+                       m)))
 
-(define (set dev r g b)
-  (blink-write dev (list 1 #x6e r g b 0 0 0 0)))
+  (define (blink-color! dev r g b)
+    (blink-write dev (list 1 #x6e r g b 0 0 0 0)))
 
-(define (blink-dev? dev)
-  (let ((desc (usb-device-descriptor dev)))
-    (and (= #x27B8 (usb-device.idVendor desc))
-         (= #x01ED (usb-device.idProduct desc)))))
+  (define (blink-dev? dev)
+    (let ((desc (usb-device-descriptor dev)))
+      (and (= #x27B8 (usb-device.idVendor desc))
+           (= #x01ED (usb-device.idProduct desc)))))
 
-(define blink-dev (find blink-dev?
-                        (usb-devices (usb-make-context))))
+  (define usb-ctx (usb-make-context))
 
-(define blink-handle (usb-open blink-dev))
-(usb-claim-interface! blink-handle)
+  (define (blink-devices)
+    (filter blink-dev? (usb-devices usb-ctx)))
 
-(display blink-dev)
-(print)
+  (define (blink-open dev)
+    (let ((dev (usb-open dev)))
+      (usb-claim-interface! dev) dev))
+)
 
-(set blink-handle 0 0 0)
+(import blink)
+
+(map (lambda (dev)
+        (blink-color! (blink-open dev)
+                      (* 8 (random 32))
+                      (* 8 (random 32))
+                      (* 8 (random 32))))
+     (blink-devices))
