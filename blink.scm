e60ffc5b123405c50e57e460a52272e2590bd457
Aaron Patterson <aaron.patterson@gmail.com>
2013-04-20 19:48:55 -0700

    can read version

--- blink.scm
+++ blink.scm
@@ -5,9 +5,10 @@
    blink-fade!
    blink-off!
    blink-write-pattern-line!
-   blink-play!)
-  (import scheme chicken ports usb srfi-1 srfi-69)
-  (use usb srfi-1 srfi-69)
+   blink-play!
+   blink-version)
+  (import scheme chicken ports usb srfi-1 srfi-69 srfi-18 posix)
+  (use usb srfi-1 srfi-69 srfi-18)
 
   (define << arithmetic-shift)
   (define (>> n count) (arithmetic-shift n (- count)))
@@ -16,10 +17,21 @@
     (with-output-to-string (lambda ()
         (for-each (lambda (byte) (write-char (integer->char byte))) buf))))
 
-  (define request-type (bitwise-ior usb::request-type-class
+  (define (drop-null buf)
+    (with-output-to-string (lambda ()
+      (for-each write-char
+                (take-while (lambda (chr) (not (= 0 (char->integer chr))))
+                            (string->list buf))))))
+
+  (define write-request (bitwise-ior usb::request-type-class
+                                     usb::recipient-interface
+                                     usb::endpoint-out))
+
+  (define read-request (bitwise-ior usb::request-type-class
                                     usb::recipient-interface
-                                    usb::endpoint-out))
+                                    usb::endpoint-in))
   (define request #x09)
+  (define get-report #x01)
   (define timeout 1000)
   (define interface-number 0)
 
@@ -28,12 +40,26 @@
                               (bitwise-and (car buf) #xFF)))
           (bytes (pack buf)))
       (usb-control-transfer handle
-                            request-type
+                            write-request
                             request
                             value
                             interface-number
                             bytes
                             timeout)))
+
+  (define (blink-read handle buf)
+    (blink-write handle buf)
+    (let ((value (bitwise-ior (<< usb::request-set-feature 8)
+                              (bitwise-and (car buf) #xFF)))
+          (bytes (pack buf)))
+      (usb-control-transfer handle
+                            read-request
+                            get-report
+                            value
+                            interface-number
+                            bytes
+                            timeout)
+      bytes))
 
   ; set the color to r g b on dev.  r g b should be values from 0 to 255
   (define (blink-set! dev r g b)
@@ -59,6 +85,16 @@
   ; Play the pattern
   (define (blink-play! dev #!optional (pos 0) (play 1))
     (blink-write dev (list 1 #x70 play pos 0 0 0 0)))
+
+  ; Get the version
+  (define (blink-version dev)
+    (let ((packet (list 1 #x76 0 0 0 0 0 0)))
+      (blink-write dev packet)
+      (let* ((str (blink-read dev packet))
+             (version (map char->integer (string->list str))))
+        (+ (* 10 (- (list-ref version 3) (char->integer #\0)))
+           (- (list-ref version 4) (char->integer #\0))))))
+        
 
   (define (blink-dev? dev)
     (let ((desc (usb-device-descriptor dev)))
@@ -91,6 +127,8 @@
 (blink-off! handle)
 (sleep 1)
 
+(test 10 (blink-version handle))
+
 ; (map (lambda (color)
 ;        (let ((r (car color)) (g (cadr color)) (b (caddr color)))
 ;          (blink-set! handle r g b)
@@ -100,23 +138,23 @@
 ; (blink-off! handle)
 ; (sleep 1)
 
-(map (lambda (color)
-       (let ((r (car color)) (g (cadr color)) (b (caddr color)))
-         (blink-fade! handle 500 r g b)
-         (sleep 1)))
-     '((255 0 0) (0 255 0) (0 0 255)))
-
-(blink-off! handle)
-(sleep 1)
-
-(define (playit handle index)
-  (if (= 100 index)
-    (blink-play! handle)
-    (begin
-      (blink-write-pattern-line! handle 100 0 255 0 index)
-      (playit handle (+ index 1)))))
-
-(playit handle 0)
+; (map (lambda (color)
+;        (let ((r (car color)) (g (cadr color)) (b (caddr color)))
+;          (blink-fade! handle 500 r g b)
+;          (sleep 1)))
+;      '((255 0 0) (0 255 0) (0 0 255)))
+; 
+; (blink-off! handle)
+; (sleep 1)
+; 
+; (define (playit handle index)
+;   (if (= 100 index)
+;     (blink-play! handle)
+;     (begin
+;       (blink-write-pattern-line! handle 100 0 255 0 index)
+;       (playit handle (+ index 1)))))
+; 
+; (playit handle 0)
 
 ; (blink-write-pattern-line! handle 100 255 0 0 0)
 ; (blink-write-pattern-line! handle 100 0 255 0 1)
